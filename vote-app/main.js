a3d34a6b35610f909141ebd21f3776d03e0085d5
Haider Malik <haidermalik504@gmail.com>
2018-09-17 16:18:55 +0500

    finish sorting and event binding

--- vote-app/main.js
+++ vote-app/main.js
@@ -2,5 +2,18 @@ new Vue({
 	el: '#app',
 	data: {
 		stories: Feed.stories
+	},
+	computed: {
+		sortedStories() {
+			return this.stories.sort((a, b) => {
+				return b.votes - a.votes;
+			});
+		}
+	},
+	methods: {
+		upVote(storyId) {
+			const index = this.stories.findIndex(story => story.id === storyId);
+			this.stories[index].votes++;
+		}
 	}
 });
