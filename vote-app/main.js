44bd36eb388d128870d935f3efa12d75cb83f934
Haider Malik <haidermalik504@gmail.com>
2018-09-17 19:00:21 +0500

    shorthand synaxt for v-bind and v-on

--- vote-app/main.js
+++ vote-app/main.js
@@ -1,13 +1,13 @@
 const StoryComponent = {
 	template: `
 	<div class="card">
-            <img class="card-img-top" v-bind:src="story.storyImage" alt="Card image cap">
+            <img class="card-img-top" :src="story.storyImage" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title"><a v-bind:href="story.url"> {{story.title}}</a></h5>
+                <h5 class="card-title"><a :href="story.url"> {{story.title}}</a></h5>
                 <p class="card-text">{{story.text}}</p>
                 <p class="card-text"><small class="text-muted">{{story.postedAt}}</small></p>
-                <button class="btn btn-primary" v-on:click="upVote(story.id)">UpVote</button>
-                <span class="badge float-right" v-bind:class="[story.votes >=10 ?  'badge-success': 'badge-secondary']">{{story.votes}}</span>
+                <button class="btn btn-primary" @click="upVote(story.id)">UpVote</button>
+                <span class="badge float-right" :class="[story.votes >=10 ?  'badge-success': 'badge-secondary']">{{story.votes}}</span>
             </div>
         </div>
 	`,
